a2a58ced30ea1eec5381d06a8035d7b64d10f7e2
andrevale99 <andrefvale@gmail.com>
2024-07-22 12:39:07 -0300

    Criação das estruturas.

--- Projeto/src/entidades.h
+++ Projeto/src/entidades.h
@@ -1,6 +1,11 @@
 #ifndef ENTIDADES_H
 #define ENTIDADES_H
 
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+//  @brief Estrutura basica para a janela principal
 struct Window
 {
     int width;
@@ -12,37 +17,57 @@ struct Window
     char *name;
 };
 
+//  @brief Estrutura para a Esfera
 typedef struct 
 {
-    float raio;
+    double raio;
 
-    //posicoes no mundo
-    int x;
-    int y;
-    int z;
+    GLint slices;
+    GLint stacks;
 
 } Esfera;
 
+//  @brief Estrutura para os planetas
 struct Planeta
 {
     Esfera esfera;
 
+    //posicoes no mundo
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
+
     float massa;
 };
 
+//  @brief Estrutura para a Pespectiva da camera
+typedef struct 
+{
+    int FOV;
+
+    int width;
+    int height;
+
+    int near;
+    int far;
+}Perspective;
+
+//  @brief Estrutura para a Camera 
 struct Camera
 {
-    int eyex;
-    int eyey;
-    int eyez;
+    GLdouble eyex;
+    GLdouble eyey;
+    GLdouble eyez;
 
-    int centerx;
-    int centery;
-    int centerz;
+    GLdouble centerx;
+    GLdouble centery;
+    GLdouble centerz;
 
-    int upx;
-    int upy;
-    int upz;
+    GLdouble upx;
+    GLdouble upy;
+    GLdouble upz;
+
+    Perspective Perspective;
 };
 
 #endif
