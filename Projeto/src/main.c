a907d8e6010d5f36488337761291c0c2a1a2fd22
andrevale99 <andrefvale@gmail.com>
2024-07-22 12:39:54 -0300

    Criação das funções de desenho no Idle, de variveis, e organização.

--- Projeto/src/main.c
+++ Projeto/src/main.c
@@ -10,6 +10,9 @@
 
 #define ESC_KEY 27
 
+//=================================================
+// VARIAVEIS
+//=================================================
 static int year = 0, day = 0;
 
 static struct Window window = {
@@ -21,14 +24,62 @@ static struct Window window = {
     .name = "Planeta",
 };
 
-static struct Camera camera = {0};
-static struct Planeta sol = {0};
+static GLfloat iluminacao[] = {0, 5.0, 0};
+GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+GLfloat mat_shininess[] = {50.0};
 
+static struct Camera camera = {0};
+
+static struct Planeta sol = {0};
+static struct Planeta mercurio = {0};
+static struct Planeta venus = {0};
+static struct Planeta terra = {0};
+static struct Planeta marte = {0};
+static struct Planeta jupiter = {0};
+static struct Planeta saturno = {0};
+static struct Planeta urano = {0};
+static struct Planeta netuno = {0};
+
+// @brief Variavel para calcular o tempo percorrido
+//
+// @note O primeiro index eh o mais recente e o segundo
+// eh o antigo
+static int deltaTime[] = {0, 0};
+//=================================================
+// PROTOTIPOS
+//=================================================
+
+// @brief Funcao de initicialização
 void init(void);
+
+// @brief Callback para desenhar os objetos
 void display(void);
+
+// @brief Callback para refazer a janela e as perspectivas
+// do programa
+//
+// @param w Largura da janela (width)
+// @param h Altura da janela (height)
 void reshape(int w, int h);
+
+// @brief Callback para capturar os eventos do teclado
+//
+// @param key Tecla pressionada
+// @param x Posicao 'x' o qual o mouse pressionou
+// @param y Posicao 'y' o qual o mouse pressionou
 void keyboard(unsigned char key, int x, int y);
 
+// @brief Callback que ira realizar as coisas
+// enquanto nao houver eventos
+void idle(void);
+
+// @brief configracao inicial dos planetas
+void set_planetas(void);
+
+// @brief Passagem de parametros ao executar o arquivo
+//
+// @param argc Quantidade de parametros
+// @param *argv[] Valores (em char** ou char *[]) dos parametros
 void Arg_parse(const int argc, char *argv[]);
 
 //=================================================
@@ -52,6 +103,7 @@ int main(int argc, char **argv)
    glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutKeyboardFunc(keyboard);
+   glutIdleFunc(idle);
 
    glutMainLoop();
 
@@ -65,20 +117,63 @@ void init(void)
 {
    glClearColor(0.0, 0.0, 0.0, 0.0);
    glShadeModel(GL_FLAT);
+
+   camera.eyex = 0.;
+   camera.eyey = 5.;
+   camera.eyez = 10.;
+   camera.upy = 1.0;
+
+   glShadeModel(GL_SMOOTH);
+
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glLightfv(GL_LIGHT0, GL_POSITION, iluminacao);
+
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
+
+   set_planetas();
 }
 
 void display(void)
 {
-   glClear(GL_COLOR_BUFFER_BIT);
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
    glColor3f(1.0, 1.0, 1.0);
 
    glPushMatrix();
-   glutWireSphere(1.0, 20, 16); /* draw sun */
+
    glRotatef((GLfloat)year, 0.0, 1.0, 0.0);
-   glTranslatef(2.0, 0.0, 0.0);
-   glRotatef((GLfloat)day, 0.0, 1.0, 0.0);
-   glutWireSphere(0.2, 10, 8); /* draw smaller planet */
+   glTranslatef(mercurio.x, mercurio.y, mercurio.z);
+   // glRotatef((GLfloat)day, 0.0, 1.0, 0.0);
+   glutSolidSphere(mercurio.esfera.raio, mercurio.esfera.slices, mercurio.esfera.stacks); /* draw smaller planet */
+   
    glPopMatrix();
+
+   glPushMatrix();
+
+   glRotatef((GLfloat)year, 0.0, 1.0, 0.0);
+   glTranslatef(venus.x, venus.y, venus.z);
+   glutSolidSphere(venus.esfera.raio, venus.esfera.slices, venus.esfera.stacks); /* draw smaller planet */
+   
+   glPopMatrix();
+
+   // Sol criado na origem do mundo
+   glPushMatrix();
+   
+   glTranslatef(sol.x, sol.y, sol.z);
+   glutSolidSphere(sol.esfera.raio, sol.esfera.slices, sol.esfera.stacks); /* draw sun */
+   
+   glPopMatrix();   
+
+   glLoadIdentity();
+   gluLookAt(camera.eyex, camera.eyey, camera.eyez,
+             camera.centerx, camera.centery, camera.centerz,
+             camera.upx, camera.upy, camera.upz);
+
+   glPopMatrix();
+
    glutSwapBuffers();
 }
 
@@ -86,11 +181,17 @@ void reshape(int w, int h)
 {
    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
    glMatrixMode(GL_PROJECTION);
+
    glLoadIdentity();
-   gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+
+   gluPerspective(60.0, (GLfloat)w / (GLfloat)h, 1.0, 30.0);
    glMatrixMode(GL_MODELVIEW);
+
    glLoadIdentity();
-   gluLookAt(0.0, 0.0, 15.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+   gluLookAt(camera.eyex, camera.eyey, camera.eyez,
+             camera.centerx, camera.centery, camera.centerz,
+             camera.upx, camera.upy, camera.upz);
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -105,12 +206,14 @@ void keyboard(unsigned char key, int x, int y)
       day = (day - 10) % 360;
       glutPostRedisplay();
       break;
-   case 'y':
-      year = (year + 5) % 360;
+
+   case '+':
+      camera.eyez = camera.eyez + 0.5;
       glutPostRedisplay();
       break;
-   case 'Y':
-      year = (year - 5) % 360;
+
+   case '-':
+      camera.eyez = camera.eyez - 0.5;
       glutPostRedisplay();
       break;
 
@@ -123,6 +226,43 @@ void keyboard(unsigned char key, int x, int y)
    }
 }
 
+void idle(void)
+{
+   deltaTime[0] = glutGet(GLUT_ELAPSED_TIME);
+
+   if ((deltaTime[0] - deltaTime[1]) > 100)
+   {
+      year = (year + 5) % 360;
+      glutPostRedisplay();
+
+      deltaTime[1] = deltaTime[0];
+   }
+}
+
+void set_planetas(void)
+{
+   sol.esfera.raio = 1.0;
+   sol.esfera.slices = 20;
+   sol.esfera.stacks = 16;
+   sol.x = 0.;
+   sol.y = 0.;
+   sol.z = 0.;
+
+   mercurio.esfera.raio = .2;
+   mercurio.esfera.slices = 10;
+   mercurio.esfera.stacks = 8;
+   mercurio.x = 3.0;
+   mercurio.y = 0.;
+   mercurio.z = 0.;
+
+   venus.esfera.raio = 0.5;
+   venus.esfera.slices = 10;
+   venus.esfera.stacks = 8;
+   venus.x = 5.0;
+   venus.y = 0.;
+   venus.z = 0.;
+}
+
 void Arg_parse(const int argc, char *argv[])
 {
    if (argc == 2 && (strcmp(argv[1], "--help") == 0))
@@ -131,7 +271,7 @@ void Arg_parse(const int argc, char *argv[])
              "--help \t Mensagem de ajuda do programa\n"
              "-w \t Largura da Janela [default=%d]\n"
              "-h \t Altura da Janela [default=%d]\n"
-             "\n" , 
+             "\n",
              window.width, window.height);
 
       exit(0);
@@ -139,7 +279,7 @@ void Arg_parse(const int argc, char *argv[])
    else
    {
       int idx = 1;
-      for(idx = 1; idx < argc; ++idx)
+      for (idx = 1; idx < argc; ++idx)
       {
          if (strcmp(argv[idx], "-w") == 0)
             window.width = atoi(argv[++idx]);
