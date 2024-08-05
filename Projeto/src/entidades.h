72d97b8280cdfa5652b54be3d46b4a8989f8c1f1
andrevale99 <andrefvale@gmail.com>
2024-08-05 09:27:43 -0300

    Criação da função para calcular a órbita do planeta.

--- Projeto/src/entidades.h
+++ Projeto/src/entidades.h
@@ -1,9 +1,13 @@
 #ifndef ENTIDADES_H
 #define ENTIDADES_H
 
+#include <math.h>
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+
+#define PI 3.14159265359
 
 //  @brief Estrutura basica para a janela principal
 struct Window
@@ -38,6 +42,11 @@ struct Planeta
     GLfloat z;
 
     float massa;
+
+    float distance;
+
+    float velocidade_x;
+    float velocidade_y;
 };
 
 //  @brief Estrutura para a Pespectiva da camera
@@ -69,5 +78,32 @@ struct Camera
 
     Perspective Perspective;
 };
+
+//  @brief Calcula a orbita do PLaneta usando o
+//  metodo Euler-Cromer
+//
+//  @param *planeta Ponteiro para o planeta
+//  @param dt Taxa de variacao do tempo
+//
+//  @note O calculo foi baseado no livro 
+//  Computitonal Physics: second edition
+//  (Nicholas J. Giordadno; Hisao Nakanishi)
+void calculo_orbita(struct Planeta *planeta, float dt)
+{
+    //variveis para guardar os valores anteriores
+    float vx_i = planeta->velocidade_x;
+    float vy_i = planeta->velocidade_y;
+    
+    float x_i = planeta->x;
+    float y_i = planeta->y;
+
+    planeta->distance = sqrt(powf(planeta->x, 2) + powf(planeta->y, 2));
+    
+    planeta->velocidade_x = vx_i - (4 * powf(PI, 2) * planeta->x) / powf(planeta->distance, 3) * dt;
+    planeta->velocidade_y = vy_i - (4 * powf(PI, 2) * planeta->y) / powf(planeta->distance, 3) * dt;
+
+    planeta->x = x_i + planeta->velocidade_x * dt;
+    planeta->y = y_i + planeta->velocidade_y * dt;
+}
 
 #endif
